1d895a678b18f66baf4009966959cf108a968e92
Evan White <ewhit6@brockport.edu>
2020-11-11 18:29:27 -0500

    fixed image display and labels, and tweaked model

--- model.py
+++ model.py
@@ -55,7 +55,7 @@ train_images = np.array(train_images).reshape(-1, 48, 48, 1)
 test_labels = np.array(test_labels)
 test_images = np.array(test_images).reshape(-1, 48, 48, 1)
 # Verify the data
-class_names = ['0', '1', '2', '3', '4', '5', '6']
+class_names = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
 
 plt.figure(figsize=(10,10))
 for i in range(25):
@@ -63,7 +63,7 @@ for i in range(25):
     plt.xticks([])
     plt.yticks([])
     plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.imshow(train_images[i], cmap='gray')
     # The CIFAR labels happen to be arrays,
     # which is why you need the extra index
     plt.xlabel(class_names[train_labels[i][0]])
@@ -71,9 +71,9 @@ plt.show()
 
 # Create the convolutional base
 model = models.Sequential()
-model.add(layers.Conv2D(48, (1, 1), activation='relu', input_shape=(48, 48, 1)))
+model.add(layers.Conv2D(48, (3, 3), activation='relu', input_shape=(48, 48, 1)))
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(96, (1, 1), activation='relu'))
+model.add(layers.Conv2D(96, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(96, (1, 1), activation='relu'))
 
@@ -81,7 +81,7 @@ model.summary()
 
 # Add dense layers on top
 model.add(layers.Flatten())
-model.add(layers.Dense(96, activation='relu'))
+model.add(layers.Dense(96 , activation='relu'))
 model.add(layers.Dense(10))
 
 model.summary()
@@ -91,7 +91,7 @@ model.compile(optimizer='adam',
             loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
             metrics=['accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=10,
+history = model.fit(train_images, train_labels, epochs=7,
                 validation_data=(test_images, test_labels))
 
 # Evaluate the model
